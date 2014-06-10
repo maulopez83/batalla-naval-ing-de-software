7ae4c7152d646b3ea1e7dadc461a777211099c65
juanjo.arce7456@gmail.com <juanjo.arce7456@gmail.com@c391ce5f-baeb-4e2d-6b09-f61a8a5d13f7>
2014-06-10 16:58:56 +0000

    Eliminación de cosas que no servían. Comentarios en las clases

--- trunk/BatallaNavalClient/src/logica/otras/MainClient.java
+++ trunk/BatallaNavalClient/src/logica/otras/MainClient.java
@@ -10,12 +10,26 @@ import logica.sockets.TalkingClientGUIObserver;
 
 public class MainClient {
 	public static void main(String[] args) throws Exception {
-		String remoteHost = "localhost";
+		/*
+		 * Inicio un listening client que escuche a mi computadora JUANJO-PC 
+		 * en el puerto 2343 donde corro el server
+		 * por ahora solo recibe el string "HolaCliente!"
+		 */
+		String remoteHost = "JUANJO-PC";
 		int remotePort = 2343;
 		ListeningClient lclient = new ListeningClient(remoteHost, remotePort);
 		new Thread(lclient).start();
+		
+		/*
+		 *Inicio un TalkingClientGUIObserver que se comunique con el servidor en JUANJO-PC
+		 *a traves del puerto 2344
+		 * 
+		 */
 		remotePort = 2344;
 		TalkingClientGUIObserver tclient = new TalkingClientGUIObserver(remoteHost, remotePort);
+		/*
+		 * Bucle infinito que envía 4 mensajes cada un segundo
+		 */
 		while(true){
 			tclient.update(new MensajeDisparo("hola", 0));
 			tclient.update(new MensajeColocar("hola", 0));
@@ -25,7 +39,4 @@ public class MainClient {
 		}
 	}	
 	
-	public static void delay(int milis) throws InterruptedException{
-		Thread.sleep(milis);
-	}
 }
