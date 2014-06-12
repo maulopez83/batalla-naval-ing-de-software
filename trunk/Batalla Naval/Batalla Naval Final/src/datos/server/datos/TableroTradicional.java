c8cab9a0a73cb866af005296d696b1c77ab441ee
juanjo.arce7456@gmail.com <juanjo.arce7456@gmail.com@c391ce5f-baeb-4e2d-6b09-f61a8a5d13f7>
2014-06-12 04:43:41 +0000

    Esta versión intercambia mensajes entre cliente y servidor, de ida y de vuelta.Tiene un prototipo de GUI. Se decodifican imagenes desde el servidor hacia el cliente para ingresar en la GUI.

---
+++ trunk/Batalla Naval/Batalla Naval Final/src/datos/server/datos/TableroTradicional.java
@@ -0,0 +1,19 @@
+package datos.server.datos;
+
+
+import java.util.ArrayList;
+
+import negocio.server.logica.*;
+
+public class TableroTradicional implements Tablero{
+	
+	public TableroTradicional(){
+		
+	}
+
+	public void setDato() {
+		//Vamos a poner el "dato" en el lugar de la coordenada pasada.
+		
+	}
+	
+}
