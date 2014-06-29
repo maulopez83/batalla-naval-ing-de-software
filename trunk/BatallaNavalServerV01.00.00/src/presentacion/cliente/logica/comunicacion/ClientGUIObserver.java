a197c1d8e5ff86e88a127e9662aa8c6ef29076a7
juanjo.arce7456@gmail.com <juanjo.arce7456@gmail.com@c391ce5f-baeb-4e2d-6b09-f61a8a5d13f7>
2014-06-29 18:35:11 +0000

    Version Funcional Server

---
+++ trunk/BatallaNavalServerV01.00.00/src/presentacion/cliente/logica/comunicacion/ClientGUIObserver.java
@@ -0,0 +1,110 @@
+package presentacion.cliente.logica.comunicacion;
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import negocio.comunicacion.mensajes.*;
+import negocio.server.logica.comunicacion.DelayThread;
+
+/*
+ * TalkingClientGUIObserver implements Observer
+ * Crea un socket con el servidor
+ * Su único metodo público es update.
+ */
+public class ClientGUIObserver implements Observer,Runnable {
+	private boolean keepGoing;
+	private Queue<Mensaje> OutputMsg;
+	private Socket connection;
+	ObjectOutputStream outToServer;
+	ObjectInputStream  inFromServer;
+	ClientMsgHandler handler;
+	/*
+	 * Constructor
+	 * params: String host, int port
+	 * Inicializa los valores de host y port para crear el socket
+	 * Inicializa OutputMsg como una nueva Cola del tipo LinkedList<Mensaje>
+	 */
+	public ClientGUIObserver(String host, int port,Subject guiSubject) throws IOException {
+		keepGoing=true;
+		OutputMsg = new LinkedList<Mensaje>();
+		guiSubject.register(this);
+		handler= new ClientMsgHandler();
+		new Thread(handler).start();
+		connection = new Socket(host, port);	
+		outToServer = new ObjectOutputStream(connection.getOutputStream());
+		inFromServer = new ObjectInputStream(connection.getInputStream());
+	}
+	/*
+	 * addMsg()
+	 * Returns: void
+	 * Params: Mensaje msg
+	 * Añade un msg a la cola de salida OutputMsg
+	 */
+	
+	private void addMsg(Mensaje msg) {
+		OutputMsg.add(msg);
+	}
+	
+	/*
+	 * update()
+	 * Returns: void
+	 * params : Mensaje m
+	 * Añade el mensaje m a la cola de salida OutputMsg
+	 * Y lo envía con la funcion sendMsg()
+	 */
+	public void update(Mensaje m) {
+		addMsg(m);
+	}
+	public void ConversarConServer(){
+		Thread Escuchar= new Thread(){		
+			public void run(){
+				while (keepGoing){
+					try{
+						Mensaje msg;
+						msg = (Mensaje) inFromServer.readObject();
+						System.out.println("Se recibio un mensaje");
+						handler.addMsg(msg);
+						System.out.println("Se decodifico el mensaje");
+					}catch(Exception e){desconectar();};
+					DelayThread.delay(100);
+			    }
+			}
+		};
+		Thread Escribir=new Thread(){
+			public void run(){
+				while (keepGoing){
+					try{
+						while(!OutputMsg.isEmpty()){
+							System.out.println("Se envia mensaje");
+							outToServer.writeObject(OutputMsg.remove());
+							DelayThread.delay(100);
+						}
+						
+					}catch(Exception e){desconectar();};
+				}
+			}
+		};
+		
+		Escuchar.start();
+		Escribir.start();
+	}
+	
+	public void desconectar() {
+		System.out.println("desconectando cliente");
+        try {
+        	outToServer.flush();
+        	outToServer.close();
+			inFromServer.close();
+			connection.close();
+			keepGoing=false;
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
+    }
+	public void run() {
+		ConversarConServer();
+		
+	}
+
+}
---
+++ trunk/BatallaNavalServerV01.00.00/src/presentacion/cliente/logica/comunicacion/ClientMsgHandler.java
@@ -0,0 +1,54 @@
+package presentacion.cliente.logica.comunicacion;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import negocio.comunicacion.mensajes.*;
+import negocio.server.logica.comunicacion.DelayThread;
+
+/*
+ * ClientMsgHandler -implements Runnable
+ * Clase que recibe mensajes en una cola de entrada MsgQ
+ * y se encarga de implementar su funcion de decodificación en el cliente
+ */
+
+public class ClientMsgHandler implements Runnable {
+	private static final long serialVersionUID = 1L;
+	public Queue <Mensaje> MsgQ;
+	
+	/*
+	 * Constructor:
+	 * Inicializa la cola de entrada MsgQ como una nueva LinkedList<Mensaje>
+	 */
+	public ClientMsgHandler(){
+		MsgQ= new LinkedList<Mensaje>();
+	}
+
+	/*
+	 * run()
+	 * returns: void
+	 * params: void
+	 * Inicia la decodificación de los mensajes
+	 * Mientras la cola MsgQ no este vacía, decodifica
+	 * cada mensaje y lo elimina de la cola.
+	 */
+	public void run() {
+		while (true){
+			while(!MsgQ.isEmpty()){
+			Mensaje m=MsgQ.remove(); //remove devuelve el mensaje en la cabeza de la cola, y lo elimina de la cola.
+			m.decodificar();
+			}
+			DelayThread.delay(100);
+		}
+	}
+	
+	/*
+	 * addMsg()
+	 * returns: void
+	 * params: Mensaje m
+	 * Añade el mensaje m a la cola de entrada MsgQ
+	 */
+	public void addMsg(Mensaje msg){
+		MsgQ.add(msg);
+	}
+	
+}
